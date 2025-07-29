6d73f348555f9d0b72a239d1a9daf49ccc1ed863
Kevin Hartman <kevin@hart.mn>
2025-07-29 14:36:08 -0400

    Reformat c files for 4-space tabs.

--- test_job_create.c
+++ test_job_create.c
@@ -7,37 +7,40 @@
 #include <string.h>
 
 extern void generate_qpy(QkCircuit *circuit, char *filename);
+
 extern void qkrt_sampler_job_write_payload(QkCircuit *circuit, int32_t shots,
-                                     char *backend, char *runtime,
-                                     char *filename);
+                                           char *backend, char *runtime,
+                                           char *filename);
+
 extern void get_access_token(void);
+
 extern void get_backend_names(void);
 
 int main(int argc, char *arv[]) {
-  get_backend_names();
-  QkCircuit *qc = qk_circuit_new(100, 100);
-  uint32_t qubits[1] = {
-      0,
-  };
-  double params[1] = {3.14159};
-  qk_circuit_gate(qc, QkGate_RZ, qubits, params);
-  qk_circuit_gate(qc, QkGate_SX, qubits, NULL);
-  qk_circuit_gate(qc, QkGate_RZ, qubits, params);
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 101; j += 1) {
-      uint32_t qubits[2] = {0, 1};
-      qk_circuit_gate(qc, QkGate_CZ, qubits, NULL);
+    get_backend_names();
+    QkCircuit *qc = qk_circuit_new(100, 100);
+    uint32_t qubits[1] = {
+            0,
+    };
+    double params[1] = {3.14159};
+    qk_circuit_gate(qc, QkGate_RZ, qubits, params);
+    qk_circuit_gate(qc, QkGate_SX, qubits, NULL);
+    qk_circuit_gate(qc, QkGate_RZ, qubits, params);
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 101; j += 1) {
+            uint32_t qubits[2] = {0, 1};
+            qk_circuit_gate(qc, QkGate_CZ, qubits, NULL);
+        }
     }
-  }
-  for (int i = 0; i < 100; i++) {
-    qk_circuit_measure(qc, i, i);
-  }
-  int32_t shots = 4196;
-  generate_qpy(qc, "test_before_json.qpy");
-  qkrt_sampler_job_write_payload(qc, shots, "ibm_pittsburgh", NULL, "test_pitt.json");
-  QkOpCounts op_counts = qk_circuit_count_ops(qc);
-  for (int i = 0; i < op_counts.len; i++) {
-    printf("%s: %lu\n", op_counts.data[i].name, op_counts.data[i].count);
-  }
-  qk_circuit_free(qc);
+    for (int i = 0; i < 100; i++) {
+        qk_circuit_measure(qc, i, i);
+    }
+    int32_t shots = 4196;
+    generate_qpy(qc, "test_before_json.qpy");
+    qkrt_sampler_job_write_payload(qc, shots, "ibm_pittsburgh", NULL, "test_pitt.json");
+    QkOpCounts op_counts = qk_circuit_count_ops(qc);
+    for (int i = 0; i < op_counts.len; i++) {
+        printf("%s: %lu\n", op_counts.data[i].name, op_counts.data[i].count);
+    }
+    qk_circuit_free(qc);
 }
--- test_job_submit.c
+++ test_job_submit.c
@@ -9,44 +9,48 @@
 typedef struct Job Job;
 
 extern int32_t qkrt_sampler_job_run(Job **job, QkCircuit *circuit, int32_t shots, char *runtime);
+
 extern void qkrt_job_free(Job *job);
+
 extern void generate_qpy(QkCircuit *circuit, char *filename);
+
 extern void get_access_token(void);
+
 extern void get_backend_names(void);
 
 int main(int argc, char *arv[]) {
-  get_backend_names();
-  QkCircuit *qc = qk_circuit_new(100, 100);
-  uint32_t qubits[1] = {
-      0,
-  };
-  double params[1] = {3.14159};
-  qk_circuit_gate(qc, QkGate_RZ, qubits, params);
-  qk_circuit_gate(qc, QkGate_SX, qubits, NULL);
-  qk_circuit_gate(qc, QkGate_RZ, qubits, params);
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 101; j += 1) {
-      uint32_t qubits[2] = {0, 1};
-      qk_circuit_gate(qc, QkGate_CZ, qubits, NULL);
+    get_backend_names();
+    QkCircuit *qc = qk_circuit_new(100, 100);
+    uint32_t qubits[1] = {
+            0,
+    };
+    double params[1] = {3.14159};
+    qk_circuit_gate(qc, QkGate_RZ, qubits, params);
+    qk_circuit_gate(qc, QkGate_SX, qubits, NULL);
+    qk_circuit_gate(qc, QkGate_RZ, qubits, params);
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 101; j += 1) {
+            uint32_t qubits[2] = {0, 1};
+            qk_circuit_gate(qc, QkGate_CZ, qubits, NULL);
+        }
     }
-  }
-  for (int i = 0; i < 100; i++) {
-    qk_circuit_measure(qc, i, i);
-  }
-  int32_t shots = 4196;
-  generate_qpy(qc, "test_before_json.qpy");
+    for (int i = 0; i < 100; i++) {
+        qk_circuit_measure(qc, i, i);
+    }
+    int32_t shots = 4196;
+    generate_qpy(qc, "test_before_json.qpy");
 
-  Job *job;
-  int res = qkrt_sampler_job_run(&job, qc, shots, NULL);
-  if (res != 0) {
+    Job *job;
+    int res = qkrt_sampler_job_run(&job, qc, shots, NULL);
+    if (res != 0) {
         printf("run failed with code: %d\n", res);
         goto cleanup;
-  }
-  qkrt_job_free(job);
-  QkOpCounts op_counts = qk_circuit_count_ops(qc);
-  for (int i = 0; i < op_counts.len; i++) {
-    printf("%s: %lu\n", op_counts.data[i].name, op_counts.data[i].count);
-  }
-  cleanup:
-  qk_circuit_free(qc);
+    }
+    qkrt_job_free(job);
+    QkOpCounts op_counts = qk_circuit_count_ops(qc);
+    for (int i = 0; i < op_counts.len; i++) {
+        printf("%s: %lu\n", op_counts.data[i].name, op_counts.data[i].count);
+    }
+    cleanup:
+    qk_circuit_free(qc);
 }
